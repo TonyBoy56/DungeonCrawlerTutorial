77014e0bbcc530c82aae0e987d06f7d2ae3426d3
Antonio Cruz <antonio.cruz5612@gmail.com>
2021-05-24 15:18:16 -0700

    add a change to classname of NewBehavior to Loader. Include check in Awake() to instantiate instance of gameManager if GameManager.instance == null.

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Loader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject gameManager;
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    void Awake()
     {
-        
+        if (GameManager.instance == null)
+        {
+            Instantiate(gameManager);
+        }
     }
 }
