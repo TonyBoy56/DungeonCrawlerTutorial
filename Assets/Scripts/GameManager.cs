8e412799ccf6da41f435aa742a9e824fbfc88835
Antonio Cruz <antonio.cruz5612@gmail.com>
2021-05-23 17:58:39 -0700

    add scripting to initialize BoardManager components on awake.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,16 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public BoardManager boardScript;
+
+    private int level = 3;
+
+    // Use this for initialization
+    void Awake ()
     {
-        
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame ()
+    {
+        boardScript.SetupScene(level);
     }
 
     // Update is called once per frame
-    void Update()
+    void Update ()
     {
         
     }
