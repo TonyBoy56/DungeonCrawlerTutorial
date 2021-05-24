9a008821beddc8e51aa49fc55e8cce60ca465642
Antonio Cruz <antonio.cruz5612@gmail.com>
2021-05-24 12:22:52 -0700

    add public static GameManager instance, establish to 'null'. Create a check in Awake() for null to create new instance, otherwise Destroy(gameObject). DontDestroyOnLoad(gameObject) when loading new scene to track scoring into new scene.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // Singleton is an object for which there can only be one instance in the game at any given time.
+    // static will declare the variable will belong to the class itself rather than an instance of the class.
+    public static GameManager instance = null;
     public BoardManager boardScript;
 
     private int level = 3;
@@ -11,6 +14,21 @@ public class GameManager : MonoBehaviour
     // Use this for initialization
     void Awake ()
     {
+        if (instance == null)
+        {
+            // create new instance of GameMananger if == null
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            // if there is an existing instance of GameManager,   we will destroy it upon load of new level to prevent two running instances
+            // GameManager is referred to as gameObject
+            Destroy(gameObject);
+        }
+        // when a new scene is loaded, normally all gameObjects in the heirarchy are destroyed. 
+        // We want to use the GameManager to keep track of score between scenes
+        // DontDestroyOnLoad allows gameObjects to persist between loaded scenes.
+        DontDestroyOnLoad(gameObject);
         boardScript = GetComponent<BoardManager>();
         InitGame();
     }
