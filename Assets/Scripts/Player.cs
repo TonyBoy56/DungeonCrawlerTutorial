e63cb199c791b2751551908d62303fef55caa123
Antonio Cruz <antonio.cruz5612@gmail.com>
2021-05-31 16:56:23 -0700

    add food as instance of playerFoodPoints. Change derivative from MonoBehavior to MovingObject() class.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,19 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MovingObject
 {
     public int wallDamage = 1;
     public int pointsPerFood = 10;
     public int pointsPerSoda = 20;
     public float restartLevelDelay = 1f;
 
+    // type specification
     private Animator animator;
     private int food;
+
     // Start is called before the first frame update
     protected override void Start()
     {
         animator = GetComponent<Animator>();
+        // set food to value of playerFoodPoints
+        food = GameManager.instance.playerFoodPoints;
+
+
     }
 
     // Update is called once per frame
