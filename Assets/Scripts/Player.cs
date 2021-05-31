227b56bf74454d4f2eee7e0ac7e8778011462f8b
Antonio Cruz <antonio.cruz5612@gmail.com>
2021-05-31 16:38:37 -0700

    add priv. int food.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     public float restartLevelDelay = 1f;
 
     private Animator animator;
+    private int food;
     // Start is called before the first frame update
     void Start()
     {
